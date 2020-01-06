00f9b9ccb3ee9387a75d6443e6a75c515ea8c397
Jonathan Aguero <jonathan@eelptex.com>
2020-01-06 19:20:57 +0100

    Test: superuser for DjangoAdmin

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 class UserManager(BaseUserManager):
+
   def create_user(self, email, password=None, **extra_fields):
     """Creates and saves a new user"""
     if not email:
@@ -10,6 +11,16 @@ class UserManager(BaseUserManager):
     user.set_password(password)
     user.save(using=self._db)
     return user
+  
+  def create_superuser(self, email, password):
+    """Creates ans saves a new super user"""
+    user = self.create_user(email, password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.save(using=self._db)
+
+    return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
   """Custom user model that supports using email instead of username"""
