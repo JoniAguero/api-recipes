76dadf521a2dba9e93a10263d5583b6209a7cbc3
Jonathan Aguero <jonathan@eelptex.com>
2020-01-10 12:32:26 +0100

    Recipe Endpoint - TDD: Models & test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -58,7 +58,7 @@ class ModelTests(TestCase):
     )
     self.assertEqual(str(ingredient), ingredient.name)
 
-  def test_recipe_stf(self):
+  def test_recipe_str(self):
     """Test the recipe string representation"""
     recipe = models.Recipe.objects.create(
       user=sample_user(),
